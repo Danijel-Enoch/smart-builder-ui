765586ec8d43ccfab50a0d15d353a67ce01a71e7
inspi-writer <conservedinnovation@gmail.com>
2023-04-21 09:12:59 +0100

    fixed zIndex in appjs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import { Layout } from "./components";
 import { BrowserRouter } from "react-router-dom";
 import { Routes } from "./config/routes";
 import bg from "./assets/image_bg.png";
-import zIndex from "@mui/material/styles/zIndex";
 
 const App = () => {
   return (
